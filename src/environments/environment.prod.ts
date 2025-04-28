df14e459f4aa2652570f1723ae33a8b10f2cb3b5
Balraj Singh <b.singh@solvingteam.it>
2025-04-28 12:02:06 +0200

    set up environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   version: '0.0.1',
 
-  baseHost: 'http://localhost:8080',
+  baseHost: 'http://35.152.218.201:8080',
   loginPath: '/api/auth/login',
 };
